ca650e2283614657136cfbc2ef280a6f6953c518
momo19971103 <momo19971103@yahoo.com.tw>
2019-10-22 12:40:33 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        private int randomNum;
+        private int inputNum;
+        private bool gameOver;
+
+        public void Play()
+        {
+            Console.WriteLine("歡迎來到我的遊戲");
+
+            Random rnd = new Random();
+            randomNum = rnd.Next(10);
+
+            while (true)
+            {
+
+                //處理輸入，並將其轉為數字
+                string input_str = Console.ReadLine();  //input  string 
+                inputNum = int.Parse(input_str);
+
+                //遊戲邏輯
+                if (randomNum == inputNum)
+                {
+                    gameOver = true;
+                }
+
+                if (gameOver)
+                {
+                    break; //離開遊戲
+                }
+            }
+
+            Console.WriteLine("你已經死了!");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一個數字: ");
+
+            string input_str = Console.ReadLine();  //input  string 
+            int input_int = int.Parse(input_str);
+            Class1 class1 = new Class1();
+            class1.Play();
+            //while 
+
+
+            Console.WriteLine("hello ");
+            Console.ReadKey();
+
+        }
+    }
+}
